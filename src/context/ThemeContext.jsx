0f6d8b6f296d47fab5bd4d878f1efd0087226a52
NoorKaram <noorkaramallah@gmail.com>
2026-06-25 19:07:51 +0300

    .

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState , useEffect} from 'react';
 
 const ThemeContext = createContext();
 export const useTheme = () => useContext(ThemeContext);
